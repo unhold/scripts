5902d8e6c67c3fab4a6722b8a0606e87408180fb
Christian Unhold <c.unhold@gmail.com>
2016-03-14 13:44:52 +0100

    Add/modify stuff.

--- fm4.sh
+++ fm4.sh
@@ -3,11 +3,9 @@
 PLS=http://mp3stream1.apasf.apa.at:8000/listen.pls
 WMA=mms://apasf.apa.at/fm4_live_worldwide
 
-FLINE=`wget -o - $PLS | grep File`
-MP3="${FLINE##*=}"
-
-URI=$MP3
+FLINE=`wget -qO- $PLS | grep File`
+URI="${FLINE##*=}"
 
 echo $URI
-gst-launch-0.10 playbin2 uri=$URI
 
+gst-launch-0.10 playbin2 uri=$URI
---
+++ listen.pls
@@ -0,0 +1,3 @@
+[playlist]
+NumberOfEntries=1
+File1=http://mp3stream1.apasf.apa.at:8000/
